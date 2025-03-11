f09cba015b05d360407a17aeaead0a5c743ac7d1
Gustavo Rodrigues <gugaaroodrigues@gmail.com>
2025-03-11 19:33:43 -0300

    Finalização exercicios de fixação

--- Construtores/ContstrutoresSobrecargaEncapsulamento/src/entities/Account.java
+++ Construtores/ContstrutoresSobrecargaEncapsulamento/src/entities/Account.java
@@ -45,5 +45,14 @@ public class Account {
 		balance -= amount + 5.0;
 	}
 	
+	public String toString() {
+		return "Account"
+				+ number
+				+ ", Holder: "
+				+ holder 
+				+", Balance: $ "
+				+ String.format("%.2f", balance);
+	}
+	
 
 }
